4705971f11cc7ee73918a7325673100b012d32a6
unknown <programista171@gmail.com>
2021-06-09 20:37:49 +0200

    Routing improved

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('journalists', JournalistsController::class);
+Route::resource('journalists', 'JournalistsController');
